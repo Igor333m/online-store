f1528ce41d622b7807133e3b3e0613b627594822
Igor Todorovic <igor333m@yahoo.com>
2019-10-20 14:17:18 -0700

    Add graphql data model and first mutation

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,18 @@
-const Mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check if the user is logged in
+
+    // createItem returns a promise
+    const item = await ctx.db.mutation.createItem({
+      data: {
+        ...args
+      }
+    }, info); // info contains query
+
+    console.log("createItem / item: ", item);
+
+    return item;
+  }
+};
 
 module.exports = Mutations;
