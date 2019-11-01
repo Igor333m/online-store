57c773d23b18eec4299dc9b4bd33be8b8c5059f3
Igor Todorovic <igor333m@yahoo.com>
2019-10-31 23:32:18 -0700

    Add item component

--- frontend/components/Items.js
+++ frontend/components/Items.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 import styled from 'styled-components';
+import Item from './Item'
 
 const ALL_ITEMS_QUERY = gql`
   query ALL_ITEMS_QUERY {
@@ -37,7 +38,9 @@ class Items extends Component {
           { ({ data, error, loading }) => {
             if (loading) return <p>Loading...</p>
             if (error) return <p>Error: {error.message}</p>
-            return <ItemsList>{data.items.map(item => <p key={item.id}>{item.title}</p>)}</ItemsList>;
+            return <ItemsList>
+            {data.items.map(item => <Item item={item} key={item.id}></Item>)}
+            </ItemsList>;
           } }
         </Query>
       </Center>
