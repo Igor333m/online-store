b39f73d2c00adc1b2b49ffcc55703ed7854cc1ce
Igor Todorovic <igor333m@yahoo.com>
2019-11-12 23:12:31 -0800

    Add file upload via cloudinary api

--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -41,6 +41,24 @@ class CreateItem extends Component {
     this.setState({ [name]: val });
   }
 
+  // Use cloudinary api for images
+  uploadFile = async e => {
+    const files = e.target.files;
+    const data = new FormData();
+    data.append('file', files[0]);
+    data.append('upload_preset', 'online-store');
+
+    const res = await fetch('https://api.cloudinary.com/v1_1/dxpvcuasb/image/upload', {
+      method: 'POST',
+      body: data
+    });
+    const file = await res.json();
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url
+    });
+  }
+
   render() { 
     return (
       <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
@@ -61,13 +79,24 @@ class CreateItem extends Component {
           }>
             <Error error={error} />
             <fieldset disabled={loading} aria-busy={loading}>
+              <label htmlFor="file">
+                Image
+                <input
+                type="file"
+                id="file"
+                name="file"
+                placeholder="Upload an image"
+                required
+                onChange={this.uploadFile} />
+                {this.state.image && <img src={this.state.image} alt="Upload Preview" />}
+              </label>
               <label htmlFor="title">
                 Title
                 <input
                 type="text"
                 id="title"
                 name="title"
-                placeholder="title"
+                placeholder="Title"
                 required
                 value={this.state.title}
                 onChange={this.handleChange} />
