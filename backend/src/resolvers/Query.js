258a0dd64b37b91e6534412b145f32bbc6becb4e
Igor Todorovic <igor333m@yahoo.com>
2020-09-19 19:28:32 -0700

    Add prisma pagination with styles

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -3,7 +3,8 @@ const { forwardTo } = require('prisma-binding');
 const Query = {
   // Forward query from joga to prisma (joga query is exactly the same)
   items: forwardTo('db'),
-  item: forwardTo('db')
+  item: forwardTo('db'),
+  itemsConnection: forwardTo('db'),
   
   // async items(parent, args, ctx, info) {
   //   console.log("getting items: ");
--- frontend/components/Items.js
+++ frontend/components/Items.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 import styled from 'styled-components';
-import Item from './Item'
+import Item from './Item';
+import Paginaton from './Pagination';
 
 // exported to search items to find deleted item
 const ALL_ITEMS_QUERY = gql`
@@ -17,6 +18,28 @@ const ALL_ITEMS_QUERY = gql`
     }
   }
 `;
+
+class Items extends Component {
+  render () {
+    return (
+      <Center>
+        <Paginaton page={this.props.page}/>
+          {/* Only child of Query Component must be a function */}
+        <Query query={ALL_ITEMS_QUERY}>
+          { ({ data, error, loading }) => {
+            if (loading) return <p>Loading...</p>
+            if (error) return <p>Error: {error.message}</p>
+            return <ItemsList>
+            {data.items.map(item => <Item item={item} key={item.id}></Item>)}
+            </ItemsList>;
+          } }
+        </Query>
+        <Paginaton page={this.props.page}/>
+      </Center>
+    );
+  }
+}
+
 const Center = styled.div`
   text-align: center;
 `;
@@ -28,26 +51,6 @@ const ItemsList = styled.div`
   max-width: ${props => props.theme.maxWidth};
   margin: auto;
 `;
-
-class Items extends Component {
-  render () {
-    return (
-      <Center>
-        <p>Items!</p>
-          {/* Only child of Query Component must be a function */}
-        <Query query={ALL_ITEMS_QUERY}>
-          { ({ data, error, loading }) => {
-            if (loading) return <p>Loading...</p>
-            if (error) return <p>Error: {error.message}</p>
-            return <ItemsList>
-            {data.items.map(item => <Item item={item} key={item.id}></Item>)}
-            </ItemsList>;
-          } }
-        </Query>
-      </Center>
-    );
-  }
-}
 
 export default Items;
 export { ALL_ITEMS_QUERY };
